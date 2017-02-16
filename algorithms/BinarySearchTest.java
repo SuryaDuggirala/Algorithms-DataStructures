05cdb8e71e12515634644e6391ab87ae1705d5a9
Surya Duggirala <suryad96@berkeley.edu>
2017-02-15 20:34:31 -0800

    Created a test for Binary Search. Needs work

---
+++ algorithms/BinarySearchTest.java
@@ -0,0 +1,20 @@
+package algorithms;
+import junit.framework.*;
+
+public class BinarySearchTest {
+    @Test
+    public void testEmptyArray() {
+        system.out.println("EMPTY LIST TEST.");
+    }
+
+    @Test
+    public void testExists() {
+        System.out.println("SORTED ARRAY BINARY SEARCH");           
+    }
+
+    @Test
+    public void testNonExistant() {
+        System.out.println("SORTED ARRAY NON EXISTANT BINARY SEARCH");    
+    }
+}
+
