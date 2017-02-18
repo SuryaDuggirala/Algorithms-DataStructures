b677a0d6276a0b77ddffe3150fdc4a4d1424a5cb
Surya Duggirala <suryad96@berkeley.edu>
2017-02-18 14:07:31 -0800

    Added a description to the head of the bin search file

--- algorithms/BinarySearch.java
+++ algorithms/BinarySearch.java
@@ -1,9 +1,23 @@
+/** The Binary Search over a sorted list of values works similar to 
+  * the way it works over a Binary Search Tree.
+  * \theta(n) = log(n)
+  * This search will check the element in the middle of the current
+  * high and low bounds and see if that value is greater than, less than
+  * or equal to the value we're seeking: the key. If it's greater than, 
+  * it will set the high bound to the middle so that it can search 
+  * for smaller numbers instead. If the number is less than, the 
+  * algorithm will adjust the low bound so that it can search for elements
+  * greater than the midpoint. It then repeats. If the midpoint is equal to 
+  * the key value, then the algorithm returns the index at which the key was 
+  * found. 
+ **/
+
 public class BinarySearch {
 
         /** What I want to do is take a sorted array A
           * and use pointers that keep track of the lowest possible 
           * value and highest possible value and find something in the 
-          * middle that might equal the value I'm trying to find.
+          * middle that might equal the value I'm trying to find.(Iterative Solution).
           **/ 
         public static int targetIndex(int target, int[] inp) {
             /** The lowest possible index that TARGET could 
